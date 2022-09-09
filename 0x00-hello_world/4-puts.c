2ddb8063380ac59ee226d6b14a32a19379fe1aa5
PitaKhing <petroxcity@gmail.com>
2022-09-09 08:11:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 /**
  * main - print the string in the put function
-(* 
  * Description: using the main function
-(* this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
-		puts("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
